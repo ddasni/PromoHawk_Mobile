7f2f0db7acca98259999c4bb6e1293a2177ee7cc
GRmegalopis <gabrielrobimlima@gmail.com>
2025-05-26 19:23:14 -0300

    adicionando imagens e a tela "CAtegoriasDestaque"

---
+++ app/src/main/java/com/example/promohawk/model/CategoriasDestaqueActivity.java
@@ -0,0 +1,18 @@
+package com.example.promohawk.model;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CategoriasDestaqueActivity {
+
+
+
+    public class CategoriasDestaqueActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_categorias_destaque);
+        }
+    }
+
+}
