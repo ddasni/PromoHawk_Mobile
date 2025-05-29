24dad519417679179edb70f5a96b99c72fdb6940
alunoinfo <promohawk01@gmail.com>
2025-05-29 14:22:10 -0300

    Fazendo a tela "Categorias em Destaque"

---
+++ app/src/main/java/com/example/promohawk/CategoriasDestaque.java
@@ -0,0 +1,14 @@
+package com.example.promohawk;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.promohawk.R;
+
+public class CategoriasDestaque extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categorias_destaque);
+    }
+}
+
