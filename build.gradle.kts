52cd053777fc38fda810fda5ae6eaefeb996d508
GRmegalopis <gabrielrobimlima@gmail.com>
2025-06-01 10:32:46 -0300

    Alterando a versão do projeto

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.3" apply false
+    id("com.android.application") version "8.9.2" apply false
     // Se usar Kotlin:
     kotlin("android") version "1.9.10" apply false
 }
@@ -11,7 +11,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.3")
+        classpath("com.android.tools.build:gradle:8.9.2")
         // Se precisar do classpath do Kotlin (só se usar Kotlin no projeto):
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     }
