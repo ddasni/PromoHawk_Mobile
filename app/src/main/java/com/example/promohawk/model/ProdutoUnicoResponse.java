4d64ae37feba2f0fd99ae28e723d164fe1eff380
ddasni <danielsousamelo5@gmail.com>
2025-06-25 14:26:43 -0300

    mandando os arquivos que faltam

---
+++ app/src/main/java/com/example/promohawk/model/ProdutoUnicoResponse.java
@@ -0,0 +1,14 @@
+package com.example.promohawk.model;
+
+public class ProdutoUnicoResponse {
+    private boolean status;
+    private Produto produto;
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+}
---
+++ app/src/main/java/com/example/promohawk/model/Usuario.java
@@ -0,0 +1,18 @@
+package com.example.promohawk.model;
+
+public class Usuario {
+    private int id;
+    private String username;
+    private String nome;
+    private String telefone;
+    private String imagem;
+    private String email;
+
+    public int getId() { return id; }
+    public String getUsername() { return username; }
+    public String getNome() { return nome; }
+    public String getTelefone() { return telefone; }
+    public String getImagem() { return imagem; }
+    public String getEmail() { return email; }
+}
+
